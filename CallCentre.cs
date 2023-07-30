e0ac0f27470b5e0c481d03d4d4fcafa73451e22f
LMVieira077 <111385062+LMVieira077@users.noreply.github.com>
2023-07-30 17:52:29 +0200

    Update CallCentre.cs
    Updated CallCentre class to better match UML diagram

--- CallCentre.cs
+++ CallCentre.cs
@@ -2,40 +2,40 @@ using System;
 
 public class CallCenter
 {
-    private DateTime timestamp;
-    private string phoneNumber;
-    private bool isNew;
+    private DateTime timeStamp;
+    private string cellNo;
+    public bool isNew;
 
-    public void AnswerCall(string phoneNumber, bool isNew)
+    public void AnswerCall(string cellNo, bool isNew)
     {
-        this.timestamp = DateTime.Now;
-        this.phoneNumber = phoneNumber;
+        this.timeStamp = DateTime.Now;
+        this.cellNo = cellNo;
         this.isNew = isNew;
 
-        Console.WriteLine($"Answering call from {phoneNumber}. Is new call: {isNew}");
+        Console.WriteLine($"Answering call from {cellNo}. Is new call: {isNew}");
     }
 
-    public void EndCall()
+    private void EndCall()
     {
-        Console.WriteLine($"Ending call from {phoneNumber}");
+        Console.WriteLine($"Ending call from {cellNo}");
         // Perform any necessary cleanup or logging here
     }
 
     public void GetPreviousRequest()
     {
         // Retrieve and display the previous request for this phone number
-        Console.WriteLine($"Getting previous request for {phoneNumber}");
+        Console.WriteLine($"Getting previous request for {cellNo}");
     }
 
     public void CreateTicket(string issueDescription)
     {
         // Create a new ticket with the provided issue description
-        Console.WriteLine($"Creating ticket for {phoneNumber} - Issue: {issueDescription}");
+        Console.WriteLine($"Creating ticket for {cellNo} - Issue: {issueDescription}");
     }
 
     public void GetClientDetails()
     {
         // Retrieve and display client details based on the phone number
-        Console.WriteLine($"Getting client details for {phoneNumber}");
+        Console.WriteLine($"Getting client details for {cellNo}");
     }
 }
