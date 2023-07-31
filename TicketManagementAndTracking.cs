ef125648ea036e33b1f71138bfb561e58348f824
EmileAFourie <109984598+EmileAFourie@users.noreply.github.com>
2023-07-31 20:54:53 +0200

    Added Functionality code to EmployeeManagement and TicketManagementandTracking
    Did extra work

--- TicketManagementAndTracking.cs
+++ TicketManagementAndTracking.cs
@@ -1,37 +1,144 @@
-using System;
-
 public class TicketManagementAndTracking
 {
-    public int ClientID { get; set; }
-    public string TypeOfError { get; set; }
-    public string Description { get; set; }
-    public string Priority { get; set; }
-    public string State { get; set; }
-    public int TicketID { get; set; }
-
-    public void LogTicket()
+   
+    private int ClientID;
+    private string TypeOfErr;
+    private string Description;
+    private DateTime DateOpened;
+    private DateTime DateClosed;
+    private string Priority;
+    private string Status;
+    private int TicketID;
+    
+    public TicketManagementAndTracking(int clientID, string typeOfErr, string description, DateTime dateOpened, string priority, string status, int ticketID)
     {
-        // Implement the logic to log the ticket and save it to the database or ticketing system
-        // For demonstration purposes, let's just display the ticket details for now
-        Console.WriteLine("Logging the ticket...");
-        Console.WriteLine($"TicketID: {TicketID}");
-        Console.WriteLine($"ClientID: {ClientID}");
-        Console.WriteLine($"TypeOfError: {TypeOfError}");
-        Console.WriteLine($"Description: {Description}");
-        Console.WriteLine($"Priority: {Priority}");
-        Console.WriteLine($"State: {State}");
-        Console.WriteLine("Ticket logged successfully!");
+        this.ClientID = clientID;
+        this.TypeOfErr = typeOfErr;
+        this.Description = description;
+        this.DateOpened = dateOpened;
+        this.Priority = priority;
+        this.Status = status;
+        this.TicketID = ticketID;
+    }
+    
+    public void LogTicket(int clientID, string typeOfErr, string description, DateTime dateOpened, string priority)
+    {
+        int newTicketID = GenerateTicketID(); 
+        TicketManagementAndTracking newTicket = new TicketManagementAndTracking(clientID, typeOfErr, description, dateOpened, priority, "Open", newTicketID);
+        allTicketsList.Add(newTicket);
+        Console.WriteLine("Ticket with ID " + newTicketID + " logged successfully.");
     }
 
-    public void ManagePriority(string newPriority)
+   public void AssignTicketToEmployee(int ticketID, int employeeID)
     {
-        // Implement the logic to update the priority of the ticket
-        // For demonstration purposes, let's just display the new priority for now
-        Console.WriteLine("Managing ticket priority...");
-        Console.WriteLine($"TicketID: {TicketID}");
-        Console.WriteLine($"Old Priority: {Priority}");
-        Console.WriteLine($"New Priority: {newPriority}");
-        Priority = newPriority; // Updating the priority field
-        Console.WriteLine("Ticket priority managed successfully!");
+        TicketManagementAndTracking ticketToAssign = FindTicketByID(ticketID);
+        if (ticketToAssign != null)
+        {
+            ticketToAssign.EmployeeID = employeeID;
+            ticketToAssign.Status = "Assigned";            
+            Console.WriteLine("Ticket with ID " + ticketID + " assigned to employee with ID " + employeeID);
+        }
+        else
+        {
+            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
+        }
+    }
+
+   public void UpdateTicketStatus(int ticketID, string status)
+    {
+        TicketManagementAndTracking ticketToUpdateStatus = FindTicketByID(ticketID);
+        if (ticketToUpdateStatus != null)
+        {
+            ticketToUpdateStatus.Status = status;
+
+            if (status == "Closed")
+            {
+                ticketToUpdateStatus.DateClosed = DateTime.Now;
+            }
+            else
+            {
+                ticketToUpdateStatus.DateClosed = null;
+            }
+            Console.WriteLine("Ticket with ID " + ticketID + " status updated to: " + status);
+        }
+        else
+        {
+            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
+        }
+    }
+
+   public static List<TicketManagementAndTracking> allTicketsList = new List<TicketManagementAndTracking>();
+
+   public TicketManagementAndTracking GetTicket(int ticketID)
+    {
+        TicketManagementAndTracking ticketToRetrieve = FindTicketByID(ticketID);
+        if (ticketToRetrieve != null)
+        {
+            return ticketToRetrieve;
+        }
+        else
+        {
+            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
+            return null;
+        }
+    }
+
+     public List<TicketManagementAndTracking> GetAllOpenTickets()
+    {
+        List<TicketManagementAndTracking> openTickets = new List<TicketManagementAndTracking>();        
+        foreach (TicketManagementAndTracking ticket in allTicketsList)
+        {
+            if (ticket.Status == "Open")
+            {
+                openTickets.Add(ticket);
+            }
+        }
+        return openTickets;
+    }
+
+    public List<TicketManagementAndTracking> GetAssignedTickets(int employeeID)
+    {
+        List<TicketManagementAndTracking> assignedTickets = new List<TicketManagementAndTracking>();        
+        foreach (TicketManagementAndTracking ticket in allTicketsList)
+        {
+            if (ticket.Status == "Assigned" && ticket.EmployeeID == employeeID)
+            {
+                assignedTickets.Add(ticket);
+            }
+        }
+        return assignedTickets;
+    }
+
+    public void ManagePriority(int ticketID, string priority)
+    {
+        TicketManagementAndTracking ticketToUpdatePriority = FindTicketByID(ticketID);
+        if (ticketToUpdatePriority != null)
+        {
+            ticketToUpdatePriority.Priority = priority;          
+            Console.WriteLine("Priority of Ticket with ID " + ticketID + " updated to: " + priority);
+        }
+        else
+        {
+            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
+        }
+    }
+
+    private static int lastGeneratedTicketID = 0; 
+    private int GenerateTicketID()
+    {
+        lastGeneratedTicketID++; 
+        return lastGeneratedTicketID;
+    }
+
+    public TicketManagementAndTracking FindTicketByID(int ticketID)
+    {
+        foreach (TicketManagementAndTracking ticket in allTicketsList)
+        {
+            if (ticket.TicketID == ticketID)
+            {
+                return ticket; 
+            }
+        }
+        return null;
     }
 }
