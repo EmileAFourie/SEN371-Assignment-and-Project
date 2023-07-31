52b7bb01061aad843deda2c17ab329181cbb9723
EmileAFourie <109984598+EmileAFourie@users.noreply.github.com>
2023-07-31 21:03:17 +0200

    Removed Code for Project1
    Removed the functionality code

--- EmployeeManagement.cs
+++ EmployeeManagement.cs
@@ -25,136 +25,36 @@ public class EmployeeManagement
     
     public void ViewEmployee(int empID)
     {
-        EmployeeManagement employee = FindEmployeeByID(empID);
-
-        if (employee != null)
-        {
-            Console.WriteLine("Employee ID: " + employee.EmpID);
-            Console.WriteLine("Name: " + employee.Name);
-            Console.WriteLine("Email: " + employee.Email);
-            Console.WriteLine("Position: " + employee.Position);
-            Console.WriteLine("Age: " + employee.Age);
-            Console.WriteLine("Performance: " + employee.Performance);
-
-            if (employee.TicketID.Count > 0)
-            {
-                Console.WriteLine("Assigned Tickets:");
-                foreach (int ticketID in employee.TicketID)
-                {
-                    Console.WriteLine("- Ticket ID: " + ticketID);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No tickets assigned.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Employee with ID " + empID + " not found.");
-        }
+        
     }
 
      public List<EmployeeManagement> ViewAllEmployees()
     {
-        List<EmployeeManagement> allEmployees = new List<EmployeeManagement>();
-
-        foreach (EmployeeManagement employee in employeesList)
-        {
-            Console.WriteLine("Employee ID: " + employee.EmpID);
-            Console.WriteLine("Name: " + employee.Name);
-            Console.WriteLine("Email: " + employee.Email);
-            Console.WriteLine("Position: " + employee.Position);
-            Console.WriteLine("Age: " + employee.Age);
-            Console.WriteLine("Performance: " + employee.Performance);
-
-            if (employee.TicketID.Count > 0)
-            {
-                Console.WriteLine("Assigned Tickets:");
-                foreach (int ticketID in employee.TicketID)
-                {
-                    Console.WriteLine("- Ticket ID: " + ticketID);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No tickets assigned.");
-            }
-
-            allEmployees.Add(employee);
-
-            Console.WriteLine("---------------------------");
-        }
-
-        return allEmployees;
+       
     }
 
     public void AddEmployee(EmployeeManagement newEmployee)
     {
-        EmployeeManagement existingEmployee = FindEmployeeByID(newEmployee.EmpID);
-        if (existingEmployee != null)
-        {
-            Console.WriteLine("Employee with ID " + newEmployee.EmpID + " already exists.");
-            return; 
-        }
-
-        employeesList.Add(newEmployee);
-        Console.WriteLine("Employee with ID " + newEmployee.EmpID + " added successfully.");
+       
     }
 
    public void DeleteEmployee(int empID)
     {
-        EmployeeManagement employeeToDelete = FindEmployeeByID(empID);
-
-        if (employeeToDelete != null)
-        {
-            employeesList.Remove(employeeToDelete);
-            Console.WriteLine("Employee with ID " + empID + " deleted successfully.");
-        }
-        else
-        {
-            Console.WriteLine("Employee with ID " + empID + " not found.");
-        }
+        
     }
 
     public void EditEmployee(int empID, EmployeeManagement updatedEmployee)
     {
-        EmployeeManagement employeeToEdit = FindEmployeeByID(empID);
-
-        if (employeeToEdit != null)
-        {
-            employeeToEdit.Name = updatedEmployee.Name;
-            employeeToEdit.Email = updatedEmployee.Email;
-            employeeToEdit.Position = updatedEmployee.Position;
-            employeeToEdit.Age = updatedEmployee.Age;
-            employeeToEdit.Performance = updatedEmployee.Performance;
-
-            Console.WriteLine("Employee with ID " + empID + " updated successfully.");
-        }
-        else
-        {
-            Console.WriteLine("Employee with ID " + empID + " not found.");
-        }
+       
     }
 
     public void AssignTicket(int ticketID)
     {
-        TicketID.Add(ticketID);
-        Console.WriteLine("Ticket with ID " + ticketID + " assigned successfully to employee with ID " + EmpID);
+       
     }
 
     public List<int> ViewActiveJobs()
     {
-        List<int> activeJobs = new List<int>();
-
-        foreach (int ticketID in TicketID)
-        {
-            if (Ticket.IsActive(ticketID))
-            {
-                activeJobs.Add(ticketID);
-            }
-        }
-
-        return activeJobs;
+       
     }
 }
--- TicketManagementAndTracking.cs
+++ TicketManagementAndTracking.cs
@@ -23,122 +23,49 @@ public class TicketManagementAndTracking
     
     public void LogTicket(int clientID, string typeOfErr, string description, DateTime dateOpened, string priority)
     {
-        int newTicketID = GenerateTicketID(); 
-        TicketManagementAndTracking newTicket = new TicketManagementAndTracking(clientID, typeOfErr, description, dateOpened, priority, "Open", newTicketID);
-        allTicketsList.Add(newTicket);
-        Console.WriteLine("Ticket with ID " + newTicketID + " logged successfully.");
+       
     }
 
    public void AssignTicketToEmployee(int ticketID, int employeeID)
     {
-        TicketManagementAndTracking ticketToAssign = FindTicketByID(ticketID);
-        if (ticketToAssign != null)
-        {
-            ticketToAssign.EmployeeID = employeeID;
-            ticketToAssign.Status = "Assigned";            
-            Console.WriteLine("Ticket with ID " + ticketID + " assigned to employee with ID " + employeeID);
-        }
-        else
-        {
-            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
-        }
+        
     }
 
    public void UpdateTicketStatus(int ticketID, string status)
     {
-        TicketManagementAndTracking ticketToUpdateStatus = FindTicketByID(ticketID);
-        if (ticketToUpdateStatus != null)
-        {
-            ticketToUpdateStatus.Status = status;
-
-            if (status == "Closed")
-            {
-                ticketToUpdateStatus.DateClosed = DateTime.Now;
-            }
-            else
-            {
-                ticketToUpdateStatus.DateClosed = null;
-            }
-            Console.WriteLine("Ticket with ID " + ticketID + " status updated to: " + status);
-        }
-        else
-        {
-            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
-        }
+        
     }
 
    public static List<TicketManagementAndTracking> allTicketsList = new List<TicketManagementAndTracking>();
 
    public TicketManagementAndTracking GetTicket(int ticketID)
     {
-        TicketManagementAndTracking ticketToRetrieve = FindTicketByID(ticketID);
-        if (ticketToRetrieve != null)
-        {
-            return ticketToRetrieve;
-        }
-        else
-        {
-            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
-            return null;
-        }
+       
     }
 
      public List<TicketManagementAndTracking> GetAllOpenTickets()
     {
-        List<TicketManagementAndTracking> openTickets = new List<TicketManagementAndTracking>();        
-        foreach (TicketManagementAndTracking ticket in allTicketsList)
-        {
-            if (ticket.Status == "Open")
-            {
-                openTickets.Add(ticket);
-            }
-        }
-        return openTickets;
+        
     }
 
     public List<TicketManagementAndTracking> GetAssignedTickets(int employeeID)
     {
-        List<TicketManagementAndTracking> assignedTickets = new List<TicketManagementAndTracking>();        
-        foreach (TicketManagementAndTracking ticket in allTicketsList)
-        {
-            if (ticket.Status == "Assigned" && ticket.EmployeeID == employeeID)
-            {
-                assignedTickets.Add(ticket);
-            }
-        }
-        return assignedTickets;
+       
     }
 
     public void ManagePriority(int ticketID, string priority)
     {
-        TicketManagementAndTracking ticketToUpdatePriority = FindTicketByID(ticketID);
-        if (ticketToUpdatePriority != null)
-        {
-            ticketToUpdatePriority.Priority = priority;          
-            Console.WriteLine("Priority of Ticket with ID " + ticketID + " updated to: " + priority);
-        }
-        else
-        {
-            Console.WriteLine("Ticket with ID " + ticketID + " not found.");
-        }
+        
     }
 
-    private static int lastGeneratedTicketID = 0; 
+  
     private int GenerateTicketID()
     {
-        lastGeneratedTicketID++; 
-        return lastGeneratedTicketID;
+       
     }
 
     public TicketManagementAndTracking FindTicketByID(int ticketID)
     {
-        foreach (TicketManagementAndTracking ticket in allTicketsList)
-        {
-            if (ticket.TicketID == ticketID)
-            {
-                return ticket; 
-            }
-        }
-        return null;
+        
     }
 }
