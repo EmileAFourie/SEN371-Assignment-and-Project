c03145cad5f6b321338050bb02175bb183260962
LMVieira077 <111385062+LMVieira077@users.noreply.github.com>
2023-08-13 19:42:20 +0200

    Added method to program.cs
    Added a method  to send a sms to client's about the status of their ticket

--- SEN371-API-Integration/Program.cs
+++ SEN371-API-Integration/Program.cs
@@ -6,8 +6,8 @@ class Program
 {
     /* string accountSid = "AC23b601fb4dbe7e1c1e5ead51dae7dba7";
     string authToken = "9790543c1792b1065cf53bdae165aef9"; */
-    string accountSid = "ACd84e3e785bb900fc451780701904ca2e"; //Leandro's Trail Account
-    string authToken = "800ed79494e6fd60772b8320edd3f60f";
+    private const string accountSid = "ACd84e3e785bb900fc451780701904ca2e"; //Leandro's Trail Account
+    private const string authToken = "770a0a9aef88cbeac2f638ffcc3ba2cf";
 
     static void Main(string[] args)
     {
@@ -41,9 +41,27 @@ class Program
         var sentMessage = MessageResource.Create(messageOptions);
     }
 
-    public bool SendClientTicket(TicketManagementAndTracking ticket){
-    
-    }
+    public static bool SendClientTicket(TicketManagementAndTracking ticket)
+        {
+            //string phone = "+27" + ticket.cellNo(1, 9);
+            string phone = "+27740136580";
+
+            try
+            {
+                TwilioClient.Init(accountSid, authToken);
+
+                var message = MessageResource.Create(
+                    body: $"\nPremier Service Solutions - There has been a update to one of your tickets: \nTicket {ticket.TicketID} has moved to {ticket.Status.ToLower()} at {ticket.DateClosed}",
+                    from: new Twilio.Types.PhoneNumber("+18156459747"),
+                    to: new Twilio.Types.PhoneNumber(phone)
+                );
+                return true;
+            }
+           catch
+           {
+               return false;
+           }
+        }
 
 }
 
